b0d8f73b491518cc121a7964c14c3c41bab78eae
Abdulbas1d <muhammadaliyevabdulbosid@gmail.com>
2024-11-14 23:21:42 +0500

    Some JavaScript codes write

---
+++ js/index.js
@@ -0,0 +1,109 @@
+const container = document.querySelector("#container");
+const reception = document.querySelector("#reception");
+const form = document.querySelector("#form");
+const img = document.querySelector("#img");
+const companyName = document.querySelector("#companyName");
+const checbox = document.querySelector("#checbox");
+const newEl = document.querySelector("#new");
+const featured = document.querySelector("#featured");
+const level = document.querySelector("#level");
+const time = document.querySelector("#time");
+const work = document.querySelector("#work");
+const addres = document.querySelector("#addres");
+const labels = document.querySelector("#labels");
+const fullstack = document.querySelector("#fullstack");
+const phyton = document.querySelector("#phyton");
+const midweight = document.querySelector("#midweight");
+const react = document.querySelector("#react");
+const button = document.querySelector("#button");
+const cards = document.querySelector("#cards");
+
+
+function createCard(data) {
+    return `
+        <div class="card">
+            <img src="${data.img}" alt="">
+            <div class="left-card">
+                <div class="companys">
+                    <span class="first">${data.name}</span>
+                    <span class="news">${data.new}</span>
+                    <span class="second">${data.featured}</span>
+                </div>
+
+                <h3 class="levels">${data.level}</h3>
+
+                <div class="times">
+                    <span>${data.time}</span>
+                    <span class="point">.</span>
+                    <span>${data.typeWork}</span>
+                    <span class="point">.</span>
+                    <span>${data.place}</span>
+                </div>
+            </div>
+
+            <div class="right-card">
+                <h4>${data.fullstack}</h4>
+                <h4>${data.midweight}</h4>
+                <h4>${data.javaScript}</h4>
+                <h4>${data.sass}</h4>
+            </div>
+        </div>
+    `
+}
+
+function validate() {
+    if (!img.value) {
+        alert("Logotip URL Manzilini to'g'ri kiritishingiz zarur!");
+        img.focus();
+        img.style.outlineColor = 'red';
+        return false
+    }
+
+    if (companyName.value.length < 5) {
+        alert("Kompaniya nomi eng kamida 5ta belgidan iborat bo'lishi kerak!");
+        companyName.focus();
+        companyName.style.outlineColor = 'red';
+        return false
+    }
+
+    if (!img.value) {
+        alert("Logotip URL Manzilini to'g'ri kiritishingiz zarur!");
+        img.focus();
+        img.style.outlineColor = 'red';
+        return false
+    }
+
+
+    return true
+}
+
+
+
+button && button.addEventListener('click', function(event) {
+    event.preventDefault()
+
+    let isValid = validate();
+    if(!isValid) {
+        return
+    }
+
+
+    const job = {
+        id: Date.now(),
+        logo: img.value,
+        companyName: companyName.value,
+        new: newEl.checked,
+        featured: featured.checked,
+        level: level.value,
+        time: time.value,
+        work: work.value,
+        addres: addres.value,
+        fullstack: fullstack.checked,
+        phyton: phyton.checked,
+        midweight: midweight.checked,
+        react: react.checked
+    }
+
+    let card = createCard(job);
+    cards.innerHTML += card;
+})
